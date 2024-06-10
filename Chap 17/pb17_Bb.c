61f5b9139ab1ee7961f290184f91d6b3ae320d81
Ashwing2002 <gadveashwin@gmail.com>
2024-06-11 00:35:06 +0530

    next commit

--- Chap 17/pb17_Bb.c
+++ Chap 17/pb17_Bb.c
@@ -1,45 +1,73 @@
 #include<stdio.h>
-struct customer
+#include<string.h>
+#include<math.h>
+int getValue(char *a);
+char* getString(int i);
+struct part
 {
-    /* data */
-    int acc;
-    char name[30];
-    int balance;
+    int year;
+    char mat[10];
+    int quantity;
 };
 
 int main(){
-    struct customer customers[200] = {0};
-    while (1)
-    {
+    struct part parts[360] = {0};
+    while(1){
         int ch;
-        printf("0.Withdrawl\n1.Deposit\n2.Exit\nEnter a choice ");
+        printf("1. Input\n2. print\n3. Print from BB1 to CC6\n4. Exit\nEnter a choice ");
         scanf("%d", &ch);
-        if(ch == 0){
-            int a;
-            printf("Enter account number ");
-            scanf("%d", &a);
-            int amount;
-            printf("Enter amount ");
-            scanf("%d", &amount);
-            if(customers[a].balance-amount < 1000){
-                printf("The balance is insufficient for the specified withdrawal\n");
-            }else{
-                customers[a].balance-=amount;
-                printf("Amount withdrawn\n");
-            }
-        }else if(ch == 1){
-            int a;
-            printf("Enter account number ");
-            scanf("%d", &a);
-            int amount;
-            printf("Enter amount ");
-            scanf("%d", &amount);
-            customers[a].balance+=amount;
-            printf("Amount deposited\n");
+        fflush(stdin);
+        if(ch == 1){
+            char sn[4];
+            printf("Enter serial number ");
+            fgets(sn, 4, stdin);
+            fflush(stdin);
+            int val = getValue(sn);
+            printf("Enter year of manufacturing, quantity ");
+            scanf("%d %d", &parts[val].year, &parts[val].quantity);
+            fflush(stdin);
+            char temp[10];
+            printf("Enter material ");
+            scanf("%s", temp);
+            strcpy(parts[val].mat, temp);
         }else if(ch == 2){
+            char sn[4];
+            printf("Enter serial number ");
+            fgets(sn, 4, stdin);
+            fflush(stdin);
+            int val = getValue(sn);
+            printf("\tSr no\tYear of Man\tMaterial\tQuantity\n\t%s\t%d\t\t%s\t\t%d\n", sn, parts[val].year, parts[val].mat, parts[val].quantity);
+        }else if(ch == 3){
+            int val1 = getValue("BB0");
+            int val2 = getValue("CC6");
+            printf("\tSr no\tYear of Man\tMaterial\tQuantity\n");
+            for(int i=0; i<360; i++){
+                if(i>=val1 && i<=val2 && parts[i].year != 0){
+                    printf("\t%s\t%d\t\t%s\t\t%d\n", getString(i), parts[i].year, parts[i].mat, parts[i].quantity);
+                }
+            }
+        }else{
             printf("Exited");
             break;
         }
+        fflush(stdin);
     }
+    
     return 0;
+}
+int getValue(char *a){
+    // printf("%s", a);
+    int ans = 0;
+    ans = 60*(a[0]-'A') + 10*(a[1]-'A') + a[2]-'0';
+    return ans;
+}
+char* getString(int i){
+    static char ans[4];
+    ans[0] = 'A'+i/60;
+    i %= 60;
+    ans[1] = 'A'+i/10;
+    i %= 10;
+    ans[2] = '0'+ i;
+    ans[3] = '\0';
+    return ans;
 }
