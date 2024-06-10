bd7c84638c0867c826fb751f79af505c2383966c
Ashwing2002 <gadveashwin@gmail.com>
2024-06-10 21:13:17 +0530

    next commit

---
+++ Chap 17/pb17_Bb.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+struct customer
+{
+    /* data */
+    int acc;
+    char name[30];
+    int balance;
+};
+
+int main(){
+    struct customer customers[200] = {0};
+    while (1)
+    {
+        int ch;
+        printf("0.Withdrawl\n1.Deposit\n2.Exit\nEnter a choice ");
+        scanf("%d", &ch);
+        if(ch == 0){
+            int a;
+            printf("Enter account number ");
+            scanf("%d", &a);
+            int amount;
+            printf("Enter amount ");
+            scanf("%d", &amount);
+            if(customers[a].balance-amount < 1000){
+                printf("The balance is insufficient for the specified withdrawal\n");
+            }else{
+                customers[a].balance-=amount;
+                printf("Amount withdrawn\n");
+            }
+        }else if(ch == 1){
+            int a;
+            printf("Enter account number ");
+            scanf("%d", &a);
+            int amount;
+            printf("Enter amount ");
+            scanf("%d", &amount);
+            customers[a].balance+=amount;
+            printf("Amount deposited\n");
+        }else if(ch == 2){
+            printf("Exited");
+            break;
+        }
+    }
+    return 0;
+}
